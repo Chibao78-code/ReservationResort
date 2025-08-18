217c0cdd7d9ab3aafe8e09683d872f9c8fadd631
Chibao78-code <ddinhchibao@gmail.com>
2025-08-18 10:05:44 +0700

    update

--- src/utils/Utils.java
+++ src/utils/Utils.java
@@ -3,11 +3,67 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package utils;
-
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.Scanner;
 /**
  *
  * @author zzzdi
  */
 public class Utils {
+    public static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+    public static String NATIONAL_ID_VALID = "^\\d{12}$";
+    public static String NAME_VALID = "^.{2,25}$";
+    public static String PHONE_VALID = "^[0]\\d{9}$";
+    public static final int MIN=1;
+    public static final int MAX=200;
     
+    public static String generateString(){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+        return LocalDateTime.now().format(formatter);
+    }
+    
+    public static String getString(String welcome) {
+        boolean check = true;
+        String result = "";
+        do {
+            Scanner sc = new Scanner(System.in);
+            System.out.print(welcome);
+            result = sc.nextLine();
+            if (result.isEmpty()) {
+                System.out.println("Input text!!!");
+            } else {
+                check = false;
+            }
+        } while (check);
+        return result;
+    }
+        public static String getString(String welcome, String format) {
+        String id;
+        boolean match;
+        while (true) {
+            Scanner sc = new Scanner(System.in);
+            System.out.print(welcome);
+            id = sc.nextLine().trim();
+            match = id.matches(format);
+            if (id.length() == 0 || match == false) {
+                System.out.println("Wrong format!");
+            } else {
+                return id;
+            }
+        }
+    }
+
+    public static String updateString(String welcome, String oldData) {
+        String result = oldData;
+        Scanner sc = new Scanner(System.in);
+        System.out.print(welcome);
+        String tmp = sc.nextLine();
+        if (!tmp.isEmpty()) {
+            result = tmp;
+        }
+        return result;
+    }
 }
